a9b2924cfbfbf058b24221e357822fdf64d5b4d6
andrea.febres <andreaefebresm@Andreas-Air.station>
2023-05-08 20:25:48 +0200

    bototni

---
+++ script.js
@@ -0,0 +1,78 @@
+function cambio1() {
+    document.getElementById("rosa1").style.color = "white";
+}
+function ricambio1() {
+    document.getElementById("rosa1").style.color = "#E6007E";
+}
+
+function cambio2() {
+    document.getElementById("rosa2").style.color = "white";
+}
+function ricambio2() {
+    document.getElementById("rosa2").style.color = "#E6007E";
+}
+
+function cambio3() {
+    document.getElementById("rosa3").style.color = "white";
+}
+function ricambio3() {
+    document.getElementById("rosa3").style.color = "#E6007E";
+}
+
+function cambio4() {
+    document.getElementById("rosa4").style.color = "white";
+}
+function ricambio4() {
+    document.getElementById("rosa4").style.color = "#E6007E";
+}
+
+
+function cambio5() {
+    document.getElementById("rosa5").style.color = "white";
+}
+function ricambio5() {
+    document.getElementById("rosa5").style.color = "#E6007E";
+}
+
+function cambio6() {
+    document.getElementById("rosa6").style.color = "white";
+}
+function ricambio6() {
+    document.getElementById("rosa6").style.color = "#E6007E";
+}
+
+function cambio7() {
+    document.getElementById("rosa7").style.color = "white";
+}
+function ricambio7() {
+    document.getElementById("rosa7").style.color = "#E6007E";
+}
+
+function cambio8() {
+    document.getElementById("rosa8").style.color = "white";
+}
+function ricambio8() {
+    document.getElementById("rosa8").style.color = "#E6007E";
+}
+
+function cambio9() {
+    document.getElementById("rosa9").style.color = "white";
+}
+function ricambio9() {
+    document.getElementById("rosa9").style.color = "#E6007E";
+}
+
+function cambio10() {
+    document.getElementById("rosa10").style.color = "white";
+}
+function ricambio10() {
+    document.getElementById("rosa10").style.color = "#E6007E";
+}
+
+function cambio11() {
+    document.getElementById("rosa11").style.color = "white";
+}
+function ricambio11() {
+    document.getElementById("rosa11").style.color = "#E6007E";
+}
+
